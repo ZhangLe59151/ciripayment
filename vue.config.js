d61b3f5924a53ee098bf00ab23d9c5c0f0dc88af
Cedric Song <songbin@silots.com>
2019-07-30 10:42:29 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -11,7 +11,7 @@ module.exports = {
       // mutate for development...
     }
   },
-  publicPath: "", // 用户部署的基本url,如果不配置默认为部署所在域的根目录，如果不要部署到子路径，则需要配置该项。如果设为空，转移后的使用相对路径引用文件。并且将所有的css js都放到了根目录
+  publicPath: "",
   outputDir: process.env.VUE_APP_OUTPUTDIR,
-  runtimeCompiler: true // 包含运行时编译器的 Vue 构建版本
+  runtimeCompiler: true
 };
