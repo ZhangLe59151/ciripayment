a10cd80dc95e5c79e59b1bddf82f458811ecb334
hans.tananda@silot.ai <hans.tananda@silot.ai>
2019-07-31 11:16:55 +0800

    update setting page

--- src/views-standard-product/Settings.vue
+++ src/views-standard-product/Settings.vue
@@ -1,3 +1,24 @@
 <template>
-
+  <div class="app-setting">
+    <app-setting-header></app-setting-header>
+  </div>
 </template>
+
+<script>
+import AppTabBar from "@/components/AppTabBar";
+import AppSettingHeader from "@/components/AppSettingHeader";
+import AppHomeBalance from "@/components/AppHomeBalance";
+import AppHomeProgress from "@/components/AppHomeProgress";
+import AppHomeDownload from "@/components/AppHomeDownload";
+export default {
+  name: "AppHome",
+
+  components: {
+    AppTabBar,
+    AppSettingHeader,
+    AppHomeBalance,
+    AppHomeProgress,
+    AppHomeDownload
+  },
+}
+</script>
