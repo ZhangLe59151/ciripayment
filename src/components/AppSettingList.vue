e1afb197dbcfcde7b2c9579fa4398d6ea050a98b
hans.tananda@silot.ai <hans.tananda@silot.ai>
2019-07-31 16:38:17 +0800

    added dialog

--- src/components/AppSettingList.vue
+++ src/components/AppSettingList.vue
@@ -2,7 +2,7 @@
   <div class="app-setting-list">
     <el-card class="box-card app-menu">
       Profile
-      <img :src="nextIcon" alt @click="$router.push({name: 'SettingsProfile'})">
+      <img :src="nextIcon" alt @click="verifyIdentity">
     </el-card>
 
     <el-card class="box-card app-menu">
@@ -28,6 +28,26 @@
       Terms & Conditions
       <img :src="nextIcon" alt>
     </el-card>
+    <el-dialog
+      :visible="profileDialog"
+      :before-close="handleClose"
+      width="90%"
+    >
+      <div class="dialog-title">View Profile</div>
+      <div class="dialog-des">
+        Some information in the profile page may be sensitive. Please verify your identity to continue.
+      </div>
+      <div class="dialog-tip">
+        Enter last 4 digits of your National ID
+      </div>
+
+      <van-password-input
+        :value="nationalID"
+        :length="4"
+        :gutter="15"
+        @focus="showKeyboard = true"
+      />
+    </el-dialog>
   </div>
 </template>
 
@@ -36,7 +56,20 @@ export default {
   name: "AppSettingList",
   data() {
     return {
-      nextIcon: require("@/assets/imgs/next.svg")
+      nextIcon: require("@/assets/imgs/next.svg"),
+      profileDialog: false,
+      nationalID: ""
+    }
+  },
+  methods: {
+    handleClose() {
+      this.profileDialog = false;
+    },
+    verifyIdentity() {
+      this.profileDialog = true;
+    },
+    handleNext() {
+      this.$router.push({ name: "SettingsProfile" });
     }
   }
 }
@@ -60,6 +93,27 @@ export default {
         display: inline-block;
         color: #87929D;
       }
+    }
+
+    .dialog-title {
+      font-size: 24px;
+      color: #000000;
+      line-height: 36px;
+      font-weight: bold;
+      margin-bottom: 20px;
+    }
+
+    .dialog-des {
+      font-size: 16px;
+      color: #000000;
+      margin-bottom: 20px;
+
+    }
+
+    .dialog-tip {
+      font-size: 14px;
+      color: #000000;
+      margin-bottom: 20px;
     }
   }
 </style>
