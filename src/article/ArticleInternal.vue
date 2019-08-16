e4c01951b243f268c7a517ff82ef5f1d603ed208
muchao <muchao@silots.com>
2019-08-16 15:53:41 +0800

    internal articles

--- src/article/ArticleInternal.vue
+++ src/article/ArticleInternal.vue
@@ -1,46 +1,68 @@
 <template>
   <section>
     <app-common-header/>
-    <div class="article">
-      <div class="title">"พระจูเลียน" รับเสพเมถุน ขอโทษคนไทย เตรียมสึกพรุ่งนี้</div>
-      <div class="date">12 July 2019</div>
-
-      <img src="@/assets/imgs/rectangle.png">
-
-      <div
-        class="cnt"
-      >Volatile Dow Jones hedge fund rates bonds fluctuate bear capital district. Securities passively taxpayer funds market dividends value financial health quarterly. Index investment performance interest potential taxpayer retirement dividends. Government credit comodity receive dividends economy. Capital exchange NYSE funds potential government federal dividends term district established economy treasury. Potential interest rate Dow Jones financial health bills NYSE exchange index funds established exchange traded funds municipal managed comodity bonds. Public notes volatile Moody's credit interest funds Fitch economy retirement.</div>
-    </div>
+    <!-- <van-overlay :show="loadingIframe" :class="{'showOverlay': loadingIframe}"></van-overlay> -->
+    <!-- <van-loading type="spinner" color="#ffffff" class="loading" v-show="loadingIframe"/> -->
+    <div v-html="articleContent">{{articleContent}}</div>
   </section>
 </template>
 
 <script>
-import AppCommonHeader from "@/components/AppCommonHeader";
 export default {
-  components: {
-    AppCommonHeader
+  data() {
+    return {
+      articleContent: "",
+      loadingIframe: true
+    };
+  },
+  methods: {
+    getArticleContent() {
+      const vm = this;
+      const id = vm.$route.query.id;
+      this.$api.getArticleContent(id).then(res => {
+        if (res.data.code === 200) {
+          this.articleContent = res.data.data.content;
+          this.loadingIframe = false;
+        } else {
+        }
+      });
+    }
+  },
+  mounted() {
+    const vm = this;
+    vm.getArticleContent();
   }
 };
 </script>
 
-<style lang="scss" scoped>
-.articleIframe {
-  height: calc(100vh - 44px);
-}
-.article {
+<style lang="scss">
+.internalArticle-article {
   margin: 16px;
   img {
     height: 160px;
     width: 100%;
   }
-  .title {
+  .showOverlay {
+    z-index: 8888 !important;
+    display: block !important;
+  }
+
+  .loading {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    margin-left: -15px;
+    margin-top: -15px;
+    z-index: 9999;
+  }
+  .internalArticle-title {
     font-weight: bold;
     font-size: 24px;
     color: #2f3941;
     letter-spacing: 0;
   }
 
-  .date {
+  .internalArticle-date {
     font-size: 16px;
     color: #68737d;
     letter-spacing: 0;
@@ -48,7 +70,7 @@ export default {
     margin: 10px 0 20px;
   }
 
-  .cnt {
+  .internalArticle-cnt {
     margin-top: 20px;
     font-size: 16px;
     color: #68737d;
