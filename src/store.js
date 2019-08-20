162fb1a9ebef66461f6e2bd95625d4ef3f5fcb53
TangXueZhi <tangxuezhi@silot.ai>
2019-08-20 23:17:38 +0800

    logout clear store

--- src/store.js
+++ src/store.js
@@ -281,6 +281,7 @@ export default new Vuex.Store({
       localStorage.setItem("logInWithPassword", "true");
     },
     logOut(state) {
+      state.userInfo.creditLimit = null;
       state.OTPVerified = false;
       state.logInWithPassword = false;
       state.fortunetellingFrame = null;
