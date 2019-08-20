d9986e746ce84f36d0189448a45b92627b126c0e
le.zhang <le.zhang@silot.ai>
2019-08-20 16:02:41 +0800

    fix a bug for addrecord

--- src/views-app/AddRecords.vue
+++ src/views-app/AddRecords.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app-add-record">
-
+{{ this.currentDate }} - {{ this.form.accountDate }}
     <app-common-header title="Add Record" />
     <van-tabs
       v-model="tabActive"
