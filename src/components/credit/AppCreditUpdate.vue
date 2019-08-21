df3df8d257c71a958336c64a89e753042236bf03
le.zhang <le.zhang@silot.ai>
2019-08-21 21:32:18 +0800

    update swipe

--- src/components/credit/AppCreditUpdate.vue
+++ src/components/credit/AppCreditUpdate.vue
@@ -1,59 +1,27 @@
 <template>
-
     <div class="app-credit-update">
       <van-swipe 
         @change="onChange"
-        :width="350"
-        :loop="false" >
+        :loop="false"
+        :show-indicators="false"
+        :width="swipeWidth" >
         <van-swipe-item>
-          <div class="box-card-1">
+          <div class="box-card">
             <div class="title">What's your monthly income?</div>
-            <div class="input_number">
-              <van-field
-              
+            <van-field
+              class="input_number"
               v-model="form.memo"
-              @focus="inputNote"
               maxlength="50"
               placeholder="5000"
-              />
-            </div>
-            <div class="error_msg">Please enter an amount</div>
-            <van-button class="submit-btn">+1,000 B criedit</van-button>
-          </div>
-        </van-swipe-item>
-        <van-swipe-item>
-          <div class="box-card-2">
-            <div class="title">What's your monthly income?</div>
-            <div class="input_number">
-              <van-field
-              
-              v-model="form.memo"
-              @focus="inputNote"
-              maxlength="50"
-              placeholder="5000"
-              />
-            </div>
-            <div class="error_msg">Please enter an amount</div>
-            <van-button class="submit-btn">+1,000 B criedit</van-button>
-          </div>
-        </van-swipe-item>
-        <van-swipe-item>
-          <div class="box-card-2">
-            <div class="title">What's your monthly income?</div>
-            <div class="input_number">
-              <van-field
-              
-              v-model="form.memo"
-              @focus="inputNote"
-              maxlength="50"
-              placeholder="5000"
-              />
-            </div>
+            />
+            <div class="curruncy">{{$store.state.currency}}</div>
+            
             <div class="error_msg">Please enter an amount</div>
             <van-button class="submit-btn">+1,000 B criedit</van-button>
           </div>
         </van-swipe-item>
       </van-swipe>
+      <div class="indicator">Swipe left to skip this question for now</div>
     </div>
 </template>
 
@@ -65,7 +33,8 @@ export default {
   data() {
     return {
       checked: true,
-      form: {}
+      form: {},
+      swipeWidth: 355
     }
   },
   computed: {
@@ -81,7 +50,7 @@ export default {
   },
   methods: {
     onChange(index) {
-      Toast('当前 Swipe 索引：' + index);
+      this.swipeWidth = 300;
     }
   }
 
@@ -93,7 +62,7 @@ export default {
     flex-grow:1;
     background-color: #04A777;
 
-    .box-card-1{
+    .box-card{
       background-color: #ffffff;
       margin: 16px 6px 0 28px;
       height: 340px;
@@ -115,16 +84,20 @@ export default {
         height: 82px;
         left: 16px;
         right: 16px;
+        width: 90%;
         top: 130px;
         font-size: 24px;
+        height: 25%;
         border-color: #B41800;
         border-style: solid;
         border-width: 1px;
         border-radius: 4px;
       }
 
-      .input {
-        
+      .curruncy {
+        position: absolute;
+        top: 163px;
+        right: 32px;
       }
 
       .error_msg {
@@ -160,78 +133,13 @@ export default {
       }
     }
 
-    .box-card-2{
-      background-color: #ffffff;
-      margin: 16px 2px 0 6px;
-      height: 340px;
-      border-radius: 10px;
-      position: relative;
-
-      .title{
-        font-size: 20px;
-        font-weight: bolder;
-        text-align: center;
-        position: absolute;
-        left: 30px;
-        right: 30px;
-        top: 30px;
-      }
-
-      .input_number {
-        position: absolute;
-        height: 82px;
-        left: 16px;
-        right: 16px;
-        top: 130px;
-        font-size: 24px;
-        border-color: #B41800;
-        border-style: solid;
-        border-width: 1px;
-        border-radius: 4px;
-      }
-
-      .input {
-        
-      }
-
-      .error_msg {
-        position: absolute;
-        top: 220px;
-        font-size: 14px;
-        color: #B41800;
-        text-align: center;
-        width: 100%;
-      }
-      
-      .submit-btn{
-        position: absolute;
-        background: #FF8600;
-        border-radius: 4px;
-        left: 16px;
-        right: 16px;
-        width: 90%;
-        height: 40px;
-        color: white;
-        font-size: 14px;
-        line-height: 40px;
-        bottom: 20px;
-      }
-      .disabled {
-        background-color: #E9EBED;
-        color: #87929D;
-      }
-      .submittedAnswer{
-        font-size: 24px;
-        position: relative;
-        top: -2px;
-      }
+    .indicator {
+      margin: 16px 0 0 0;
+      font-size: 14px;
+      text-align: center;
+      color: white;
     }
-  }
 
-  .test {
-    width: 800px;
-    height: 100px;
-    background-color: white;
   }
 </style>
 
