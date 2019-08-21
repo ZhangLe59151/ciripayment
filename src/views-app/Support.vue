f674eebfbb32f8085b032e2170852a726dff279e
le.zhang <le.zhang@silot.ai>
2019-08-21 16:34:00 +0800

    update support

--- src/views-app/Support.vue
+++ src/views-app/Support.vue
@@ -2,13 +2,34 @@
   <div class="app-support">
     <app-common-header title="Support" />
     <div class="feedback">
+      <div class="feedback_title">
       Got feedback or 
       experiencing problems?
-      <div>Drop us a message and our customer
+      </div>
+      <div class="feedback_word">
+        Drop us a message and our customer
           support representatives will be happy to
           assist you!
       </div>
     </div>
+
+    <div class="label_title">Your Email</div>
+
+    <van-field  
+      v-model="email" 
+      placeholder="email@domain.com" />
+
+    <div class="label_title">Your Message</div>
+
+    <textarea 
+      class="textarea"
+      maxlength="200" 
+      @input="descInput" 
+      v-model="desc"
+      placeholder="Describe your problem or feedback" />
+
+    <div>{{remnant}}/500</div>
+
   </div>
 </template>
 
@@ -17,13 +38,18 @@ export default {
   name: "Support",
   data() {
     return {
-      profileDialog: false,
-      showKeyboard: false,
-      value: "",
-      login: true
+      email: "",
+      login: true,
+      remnant: 500,
+      desc: ""
     };
   },
   methods: {
+    descInput(){
+      var txtVal = this.desc.length;
+      this.remnant = 200 - txtVal;
+      remnant: 500;
+      }
   }
 };
 </script>
@@ -31,39 +57,41 @@ export default {
 <style lang="scss" scoped>
 .app-support {
   margin: 0 0 0 0;
-  position: relative;
 
-  .dialog-title {
-    font-size: 24px;
-    color: #000000;
-    line-height: 36px;
-    font-weight: bold;
-    margin-bottom: 20px;
-  }
+  .feedback {
+    background-color: #c8c8c8;
+    height: 184px;
+    margin: 0 0 0 0;
+    position: relative;
 
-  .van-cell-group {
-    margin-top: 20px;
-  }
-  .dialog-des {
-    font-size: 16px;
-    color: #000000;
-    margin-bottom: 20px;
-  }
+    .feedback_title {
+      position: absolute;
+      font-size: 24px;
+      font-weight: bolder;
+      left: 16px;
+      right: 16px;
+      top: 20px;
+    }
 
-  .dialog-tip {
-    font-size: 14px;
-    color: #000000;
-    margin-bottom: 20px;
-  }
-  .van-number-keyboard {
-    z-index: 3001 !important;
+    .feedback_word {
+      position: absolute;
+      font-size: 16px;
+      left: 16px;
+      right: 16px;
+      top: 96px;
+    }
   }
 }
 
-.setting_title {
+.label_title {
+    margin: 16px 16px 0 16px;
+    height: 24px;
+  }
+
+.textarea {
   margin: 16px 16px 4px 16px;
-  font-size: 12px;
-  font-weight: bolder;
+  height: 144px;
+  font-size: 16px;
   color: #2F3941;
 }
 </style>
