84d9bba45d15bd88bb95ff8afdbf14035eaaf51c
Cedric Song <songbin@silots.com>
2019-08-22 17:51:20 +0800

    update

--- src/views-app/Credit.vue
+++ src/views-app/Credit.vue
@@ -2,14 +2,9 @@
   <div class="credit">
     <app-common-header title="Credit Line" />
 
-<<<<<<< HEAD
     <credit-app-credit-limit />
-    <credit-app-credit-update v-if="!finishedAll" />
-=======
-   <credit-app-credit-limit />
 
-    <credit-app-credit-update v-if="!finishedAll"/>
->>>>>>> c3ed0071cbbda39670398ac123f60ac53a1f17e3
+    <credit-app-credit-update v-if="!finishedAll" />
     <credit-app-credit-finish-questions v-else />
   </div>
 </template>
@@ -19,17 +14,13 @@ import { mapState } from "vuex";
 export default {
   name: "Credit",
   data() {
-    return {
-<<<<<<< HEAD
-      finishedAll: false
-    };
-=======
-    }
->>>>>>> c3ed0071cbbda39670398ac123f60ac53a1f17e3
+    return {};
   },
   computed: {
     ...mapState({
-      finishedAll: state => state.credit.questions.map(item => item.value).filter(Boolean).length === 3,
+      finishedAll: state =>
+        state.credit.questions.map(item => item.value).filter(Boolean)
+          .length === 3,
       questions: state => state.credit.questions
     })
   },
@@ -42,18 +33,17 @@ export default {
     //   }
     // });
 
-<<<<<<< HEAD
     const creditMock = {
       currentCreditLimit: 50000,
       questions: [
         {
-          id: 1,
-          question: "Do you have a driving licence?",
-          type: 3,
-          options: ["Yes", "No"],
-          limitAmount: 5000,
-          value: null,
-          placeholder: ""
+          id: 6,
+          question: "What is your monthly expenses?",
+          type: 1,
+          options: null,
+          limitAmount: 7000,
+          value: "",
+          placeholder: "25000"
         },
         {
           id: 5,
@@ -76,46 +66,6 @@ export default {
       ]
     };
 
-    let numberOfAnswered = creditMock.questions
-      .map(item => item.value)
-      .filter(Boolean).length;
-    this.finishedAll = numberOfAnswered === 3;
-=======
-    const creditMock =
-      {
-        currentCreditLimit: 50000,
-        questions: [
-          {
-            "id": 6,
-            "question": "What is your monthly expenses?",
-            "type": 1,
-            "options": null,
-            "limitAmount": 7000,
-            "value": "",
-            "placeholder": "25000"
-          },
-          {
-            "id": 5,
-            "question": "What is your monthly income?",
-            "type": 1,
-            "options": null,
-            "limitAmount": 7000,
-            "value": "",
-            "placeholder": "25000"
-          },
-          {
-            "id": 9,
-            "question": "What do you sell or provide?",
-            "type": 2,
-            "options": null,
-            "limitAmount": 6000,
-            "value": "",
-            "placeholder": "E.g. Sell clothes, sell food"
-          }
-        ]
-      };
-
->>>>>>> c3ed0071cbbda39670398ac123f60ac53a1f17e3
     this.$store.commit("InitCredit", creditMock);
   }
 };
