4e358abec5c817a2c488b08739790dc52adfbb17
le.zhang <le.zhang@silot.ai>
2019-08-23 10:38:54 +0800

    udpate

---
+++ src/views-app/Credit.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="credit">
+    <van-nav-bar
+      @click-left="$router.push({name: 'Home'})"
+      :border="false"
+      left-arrow
+    >
+      <div
+        slot="title"
+        class="header-title"
+      >
+        Credit Line
+      </div>
+    </van-nav-bar>
+
+   <app-credit-limit />
+    <app-credit-update />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Credit",
+  created() {
+    this.$store.commit("fetchCreditDataFromLocal");
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .credit{
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
