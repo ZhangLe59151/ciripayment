9e5867cbd707f327748faf6a4a087a54d2ac4ebe
Cedric Song <songbin@silots.com>
2019-08-27 11:02:03 +0800

    update

--- src/components/fortune-telling/AppFortuneHeader.vue
+++ src/components/fortune-telling/AppFortuneHeader.vue
@@ -1,12 +1,34 @@
 <template>
   <div class="app-fortune-header">
-    <div class="title">Daily Fortune</div>
-    <van-icon name="cross" class="close" @click="returnHome" size="20px"></van-icon>
+    <div class="title">{{title}}</div>
+    <van-icon
+      name="arrow-left"
+      class="arrow-left"
+      @click="$router.back()"
+      v-if="showKLeftIcon"
+    />
+    <van-icon
+      name="cross"
+      class="close"
+      @click="returnHome"
+      v-if="showRightIcon"
+    />
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    title: {
+      default: "Daily Fortune"
+    },
+    showRightIcon: {
+      default: true
+    },
+    showKLeftIcon: {
+      default: false
+    }
+  },
   methods: {
     returnHome() {
       this.$router.push({ name: "Home" });
@@ -16,23 +38,32 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .app-fortune-header {
-    height: 70px;
-    position: relative;
-    .close {
-      position: absolute;
-      right: 16px;
-      color: white;
-      line-height: 70px;
-    }
-    .title {
-      position: absolute;
-      text-align: center;
-      line-height: 70px;
-      font-weight: bold;
-      font-size: 24px;
-      color: white;
-      width: 100%;
-    }
+.app-fortune-header {
+  height: 70px;
+  position: relative;
+
+  .arrow-left {
+    position: absolute;
+    left: 16px;
+    color: white;
+    line-height: 70px;
+    font-size: 20px;
   }
+  .close {
+    position: absolute;
+    right: 16px;
+    color: white;
+    line-height: 70px;
+    font-size: 20px;
+  }
+  .title {
+    position: absolute;
+    text-align: center;
+    line-height: 70px;
+    font-weight: bold;
+    font-size: 24px;
+    color: white;
+    width: 100%;
+  }
+}
 </style>
---
+++ src/views-app/fortune/MasterProfile.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="master-profile">
+    <fortune-telling-app-fortune-header
+      :showKLeftIcon="true"
+      title="Master's Profile"
+    />
+  </div>
+</template>
