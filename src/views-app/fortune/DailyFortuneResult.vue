4bc1d31cde6b41399d6d12ffca014a3c10150eb9
Cedric Song <songbin@silots.com>
2019-08-27 20:26:17 +0800

    update share

--- src/views-app/fortune/DailyFortuneResult.vue
+++ src/views-app/fortune/DailyFortuneResult.vue
@@ -8,11 +8,38 @@
       :fortuneInfo="fortuneInfo"
       :currency="currency"
     />
-    <div class="like"
-         @click="triggerLike">
-      <i :class="likeStatus ? 'iconfont iconlike' : 'iconfont iconunlike'" />
-      <span class="like-text">Like</span>
-    </div>
+
+    <section class="action">
+      <div
+        class="like"
+        @click="triggerLike"
+      >
+        <i :class="likeStatus ? 'iconfont iconlike' : 'iconfont iconunlike'" />
+        <span class="like-text">Like</span>
+      </div>
+      <div
+        class="like"
+        @click="triggerShare"
+      >
+        <i :class="likeStatus ? 'iconfont iconlike' : 'iconfont iconunlike'" />
+        <span class="like-text">Share</span>
+      </div>
+    </section>
+
+    <van-popup
+      v-model="showPopUp"
+      round
+      :overlay="false"
+      position="bottom"
+      get-container="daily-fortune-result"
+      class="popup"
+    >
+      <div class="share-text">{{$t("FortuneTelling.shareCopy")}} </div>
+      <div>
+        <img :src="require('@/assets/imgs/fortune-telling/line_ico.png')">
+        <img :src="require('@/assets/imgs/fortune-telling/facebook_ico.png')">
+      </div>
+    </van-popup>
   </div>
 </template>
 
@@ -22,6 +49,7 @@ export default {
   name: "DailyFortuneResult",
   data() {
     return {
+      showPopUp: false,
       fortuneResult: "",
       salesResult: "",
       likeStatus: false,
@@ -32,7 +60,8 @@ export default {
     ...mapState({
       fortuneInfo: "fortuneInfo",
       localDateFormatter: "localDateFormatter",
-      currency: "currency"
+      currency: "currency",
+      deviceType: "deviceType"
     }),
     today() {
       return this.$moment().format(this.localDateFormatter);
@@ -44,13 +73,20 @@ export default {
     this.likeStatus = savedResult.like;
   },
   destroyed() {
-    this.updateLikeStatusWithApi()
+    this.updateLikeStatusWithApi();
   },
   methods: {
+    triggerShare() {
+      this.showPopUp = true;
+      const funcName = "shareOn".concat(this.deviceType);
+      this[funcName]();
+    },
+    shareOnAPP() {},
+    shareOnWEB() {},
     triggerLike() {
       this.likeStatus = !this.likeStatus;
       if (!this.processingLike) {
-        this.updateLikeStatusWithApi()
+        this.updateLikeStatusWithApi();
       }
     },
     updateLikeStatusWithApi() {
@@ -76,7 +112,8 @@ export default {
   position: relative;
   height: 100vh;
   width: 100%;
-  background: no-repeat center url("../../assets/imgs/fortune-telling/fortune_telling_bg.png");
+  background: no-repeat center
+    url("../../assets/imgs/fortune-telling/fortune_telling_bg.png");
   background-size: cover;
   .top-desc {
     position: absolute;
@@ -89,21 +126,59 @@ export default {
   .app-fortune-result-content {
     margin-top: 20px;
   }
+
+  .action {
+    display: flex;
+    flex: 1 1;
+    margin: 0 66px;
+    justify-content: space-between;
+  }
+
+  .share {
+    color: white;
+    margin-top: 40px;
+    text-align: right;
+    margin-right: 20px;
+    font-size: 40px;
+  }
+
   .like {
     color: white;
     margin-top: 70px;
-    text-align: right;
+    text-align: left;
     margin-right: 20px;
+
     .iconunlike {
       color: white;
+      font-size: 30px;
     }
     .iconlike {
-      color: #D44832;
+      color: #d44832;
+      font-size: 30px;
     }
     .like-text {
       color: white;
       font-size: 16px;
       margin-left: 8px;
+    }
+  }
+
+  .popup {
+    height: 130px;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+
+    .share-text {
+      font-size: 16px;
+      color: #2f3941;
+      letter-spacing: 0;
+      margin: 20px 20px 10px 20px;
+    }
+
+    img {
+      height: 40px;
+      width: 40px;
+      margin: 10px 0 0 20px;
     }
   }
 }
