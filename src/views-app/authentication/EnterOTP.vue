95793cfdc4a67849af865dcbedb262a3d55763af
le.zhang <le.zhang@silot.ai>
2019-08-27 15:55:36 +0800

    update otp

--- src/views-app/authentication/EnterOTP.vue
+++ src/views-app/authentication/EnterOTP.vue
@@ -1,85 +1,27 @@
 <template>
-  <div class="enter-otp">
-    <WapHeader
-      :left="true"
+ <div class="enterOtpPage">
+    <!-- <WapHeader
       :center="true"
-    />
-
-    <div class="login-title center">
-      Log In
-    </div>
-
-    <div class="enterOtpWrapper">
-      <div class="sub-tips">
-        Your mobile number
-      </div>
-
-      <div class="otp">
-        <el-form
-          label-width="0px"
-          :model="form"
-          ref="elForm"
-          size="small"
-          style="margin-top: 15px;"
-        >
-          <el-form-item
-            label-width="80px"
-            class="enterotp"
-            prop="phone"
-          >
-            <span slot="label">
-              <span style="margin: 0 10px;">{{form.nationalCode}}</span>
-              <i
-                class="el-icon-caret-bottom"
-                style="color: #929292"
-              ></i>
-            </span>
-            <el-input
-              v-model="form.phone"
-              :maxlength="this.$store.state.phone.maxLen"
-              :minlength="this.$store.state.phone.minLen"
-              disabled
-            ></el-input>
-          </el-form-item>
-        </el-form>
-      </div>
-
-      <div
-        v-if="!sendOtpError"
-        class="tips"
-      >A One-Time-Passcode (OTP) has been sent to the mobile number above.
-      </div>
-      <div
-        v-else
-        class="tips-error"
-      >{{otpErrorMsg}}
-      </div>
-      <div class="sub-tips">Enter OTP</div>
-
+      style="top: 34px"
+    /> -->
+    <div class="landingPageContent">
+      <div class="slogan-title">A One-Time-Passcode (OTP) has been sent to {{ phone }} </div> 
+      <div class="slogan-sub">Enter OTP</div>
       <van-password-input
-        v-if="form.accountVerified"
+        class="inputCode"
         :value="value"
         info
-        @focus="showKeyboard = true"
+        @focus="showNumber = true"
         :mask="false"
         :length="6"
-        style="margin-left: -15px ; color : #929292;"
-      />
-      <van-password-input
-        v-else
-        :value="value"
-        info
-        @focus="showKeyboard = true"
-        :mask="false"
-        :length="6"
-        style="margin-left: -15px ; color : #929292;"
+        gutter=8
       />
 
       <div
         v-show="!show"
         class="resend"
-      >Resend OTP（{{count}}s）
-      </div>
+      >Resend OTP（{{count}}s）</div>
+
       <div
         class="resend-active"
         v-show="show"
@@ -87,105 +29,19 @@
       >Resend OTP
       </div>
 
-      <van-button
-        v-if="!form.accountVerified"
-        size="large"
-        class="bottom-btn"
-        @click="handleVerifyOtpUnverifiedAccount"
-      >
-        Continue
-      </van-button>
+
     </div>
 
-    <!--    <div class="page">3/3</div>-->
-    <!--    &lt;!&ndash; 数字键盘 &ndash;&gt;-->
     <van-number-keyboard
-      :show="showKeyboard"
+      :show="showNumber"
+      close-button-text="Done"
+      @blur="showNumber = false"
       @input="onInput"
       @delete="onDelete"
-      @blur="showKeyboard = false"
     />
+
   </div>
 </template>
-
-<style lang="scss" scoped>
-@import "../../assets/css/bottom-btn";
-
-.enter-otp {
-  background: url("../../assets/imgs/landing_bg.png");
-  min-height: 100vh;
-  .login-title {
-    font-size: 24px;
-    color: #ffffff;
-    font-weight: bold;
-    margin-top: 3.25rem;
-  }
-
-  .enterOtpWrapper {
-    background-color: #ffffff;
-    position: fixed;
-    bottom: 0;
-    height: 72vh;
-    border-top-left-radius: 16px;
-    border-top-right-radius: 16px;
-    padding: 20px 20px 0;
-  }
-
-  .tips {
-    font-size: 16px;
-    margin-top: 2.25rem;
-    margin-bottom: 2.25rem;
-  }
-  .tips-error {
-    font-size: 20px;
-    padding: 20px;
-    color: #dd1111;
-  }
-
-  .sub-tips {
-    font-size: 14px;
-    margin: 10px 0;
-  }
-
-  .phone {
-    font-size: 27px;
-    text-decoration: underline;
-  }
-
-  .otp {
-    text-align: left;
-  }
-
-  .resend {
-    margin: 20px 0;
-    text-decoration: none;
-    font-size: 14px;
-    color: #87929d;
-  }
-  .resend-active {
-    margin: 20px 0;
-    text-decoration: none;
-    font-size: 14px;
-    color: #037aff;
-  }
-
-  .page {
-    position: fixed;
-    width: 100%;
-    text-align: center;
-    bottom: 20px;
-  }
-  .bottom-btn {
-    background-color: #ff8600;
-    border-radius: 4px;
-    margin: 0;
-    width: calc(100% - 40px);
-    position: absolute;
-    bottom: 20px;
-    left: 20px;
-  }
-}
-</style>
 
 <script>
 // @ is an alias to /src
@@ -199,9 +55,9 @@ export default {
   data() {
     return {
       active: 2,
-      phone: "",
+      phone: this.$store.state.userInfo.nationalCode + " " + this.$store.state.userInfo.phone,
       value: "",
-      showKeyboard: true,
+      showNumber: true,
       show: true,
       count: "",
       timer: null,
@@ -218,12 +74,13 @@ export default {
     }
   },
   watch: {
-    value: function(val) {
+    /*value: function(val) {
       // watch input if verified account
+      debugger
       if (this.form.accountVerified) {
         this.handleVerifyOtpVerifiedAccount();
       }
-    }
+    }*/
   },
   created() {
     this.countTime();
@@ -287,6 +144,9 @@ export default {
     },
     onInput(key) {
       this.value = (this.value + key).slice(0, 6);
+      if (this.value.length === 6) {
+        this.handleVerifyOtpUnverifiedAccount();
+      }
     },
     onDelete() {
       this.value = this.value.slice(0, this.value.length - 1);
@@ -402,3 +262,70 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+
+.enterOtpPage {
+  position: fixed;
+  background: url("../../assets/imgs/landing_bg.png") no-repeat;
+  background-size: cover;
+  height: 100vh;
+
+  .landingPageContent {
+    position: relative;
+    margin: 100px 16px 0 16px;
+    background-color: white;
+    height: 258px;
+    width: 328px;
+    border-radius: 16px;
+
+    .slogan-title {
+      position: absolute;
+      margin: 24px 16px 0 16px;
+      font-size: 20px;
+      font-weight: bolder;
+      color: #2F3941;
+      text-align: left;
+    }
+
+    .slogan-sub {
+      position: absolute;
+      text-align: left;
+      color: #2F3941;
+      font-size: 14px;
+      top: 120px;
+      left: 16px;
+    }
+
+    .inputCode {
+      position: absolute;
+      top: 145px;
+    }
+
+    .van-hairline {
+      border: 1px solid #2F3941;
+      border-radius: 4px;
+      margin: 0 2px 0 2px;
+    }
+
+    .resend {
+      position: absolute;
+      left: 16px;
+      bottom: 24px;
+      font-size: 14px;
+      color: #87929d;
+    }
+
+    .resend-active {
+      position: absolute;
+      left: 16px;
+      bottom: 24px;
+      text-decoration: none;
+      font-size: 14px;
+      color: #037aff;
+    }
+      
+  }
+
+}
+</style>
