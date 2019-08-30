8efc17d8102396572f23abbd27d881ca4d994bc5
Cedric Song <songbin@silots.com>
2019-08-30 16:49:41 +0800

    update

--- src/views-app/TermsAndConditions.vue
+++ src/views-app/TermsAndConditions.vue
@@ -1,152 +1,41 @@
 <template>
-  <div class="tandc">
+  <div>
     <app-common-header title="Terms & Conditions" />
-    <div class="wrapper">
-      <div class="title"></div>
-      <div class="update-time">
-        <div class="k">Last Updated:</div>
-        <div class="v">13 Apr 2019</div>
-      </div>
-      <div style="clear:both"></div>
-      <div class="content">
-        <div class="sub-title">Information Collection</div>
-        <p>In order to provide a better service to you, we may collect and process the following data about you:</p>
-        <ol>
-          <li>Information that you provide by filling in forms on our site www.silots.com ("our site"). This includes information provided at the time of registering to use our site, subscribing to our service, posting material or requesting further services. We may also ask you for information at other times, for example in connection with a promotion or when you report a problem with our site;</li>
-          <li>If you contact us, we may keep a record of that correspondence;</li>
-          <li>Details of any future transactions you may carry out through our site and of the fulfillment of your orders;</li>
-          <li>Details of your visits to our site and the resources that you access.</li>
-        </ol>
-        <p>We only retain personal data for so long as it is necessary. Data may be archived as long as the purpose for which the data was used still exists.</p>
-      </div>
-      <div class="content">
-        <div class="sub-title">Uses of the Information</div>
-        <p>The purposes for which information may be used by us in and outside Singapore include:</p>
-        <ol>
-          <li>Ensuring that content from our site is presented in the most effective manner for you and for your computer;</li>
-          <li>Providing you with alerts, newsletter, education materials or information that you requested or signed up to;</li>
-          <li>Carrying out our obligations arising from any contracts entered into between you and us;</li>
-          <li>Allowing you to participate in interactive features of our service when you choose to do so;</li>
-          <li>Designing and conducting surveys/questionnaires for client profiling/segmentation, statistical analysis, improving and furthering the provision our products and services;</li>
-          <li>Complying with laws and regulations applicable to us or any of our affiliates in or outside Singapore;</li>
-          <li>Legal proceedings, including collecting overdue amounts and seeking professional advice;</li>
-          <li>Researching, designing and launching services or products including seminars/events/forums;</li>
-          <li>Promoting and marketing services and products subject to your exercise of the opt-out right (please see further details in Clause 2.2 below); or purposes directly related or incidental to the above.</li>
-        </ol>
-        <p>We intend to use your data in direct marketing and we require your consent (which includes an indication of no objection) for that purpose. In this connection, please note that:</p>
-        <ol>
-          <li>Your name, contact details (including address, contact number, email address), products and services information, transaction pattern and behavior, background and demographic data held by us from time to time may be used by us in direct marketing;</li>
-          <li>
-            The following classes of services, products, and subjects may be marketed in direct marketing:
-            <ol class="sub-list">
-              <li>Services and products related to our site and/or our affiliates (including marketing affiliates programs we are a part of);</li>
-              <li>Reward, loyalty or privileges programmes, promotional offers and related services;</li>
-              <li>Invitations to events such as seminars/webinars/teleseminars, conferences, live programs or events;</li>
-            </ol>
-          </li>
-          <li>We may conduct direct marketing via fax, email, direct mail, telephone and other means of communication or send e-newsletters to you. You may choose not to receive promotional materials, by simply telling us (see below for contact details), and we will cease to do so, without charge.</li>
-        </ol>
-      </div>
-      <div class="content">
-        <div class="sub-title">Disclosure of Your Information</div>
-        <p>We will keep the personal data we hold confidential but may provide information to:</p>
-        <ol>
-          <li>Personnel, agents, advisers, auditors, contractors, financial institutions, and service providers in connection with our operations or services;</li>
-          <li>Our overseas offices, affiliates, business partners and counterparts (if any);</li>
-          <li>Persons under a duty of confidentiality to us;</li>
-          <li>Persons to whom we are required to make disclosure under applicable laws and regulations in or outside Singapore;</li>
-          <li>Actual or proposed transferees or participants of our services in or outside Singapore.</li>
-        </ol>
-        <p>We only retain personal data for so long as it is necessary. Data may be archived as long as the purpose for which the data was used still exists.</p>
-      </div>
-      <div class="content">
-        <div class="sub-title">Security</div>
-        <p>All information you provide to us is stored on our secure servers. Where we have given you (or where you have chosen) a password which enables you to access certain parts of our site, you are responsible for keeping this password confidential. We ask you not to share a password with anyone.</p>
-        <p>Unfortunately, the transmission of information via the internet is not completely secure. Although we will do our best to protect your personal data, we cannot guarantee the security of your data transmitted to our site; any transmission is at your own risk and you agree not to hold us responsible for any breach of security while accessing the internet that is out of our control. Once we have received your information, we will use strict procedures and security features to try to prevent unauthorized access.</p>
-      </div>
-      <div class="content">
-        <div class="sub-title">Third Party Websites</div>
-        <p>Our website may contain links to other sites (the "Linked Sites"). We are not responsible for the privacy policies or practices of such other Linked Sites. We encourage our users to be aware when leaving our website to read the privacy policies or statements of each Linked Site that collects your personal data. You agree to waive any claim against us with respect to the Linked Sites.</p>
-      </div>
-      <div class="content">
-        <div class="sub-title">Changes to Our Privacy Policy</div>
-        <p>Any changes we may make to our Privacy Policy in the future will be posted on this page. Your continued use of our services constitutes your agreement to this Privacy Policy and any updates.</p>
-      </div>
-      <div class="content">
-        <div class="sub-title">Data Transfers</div>
-        <p>We may hold your data on servers in Singapore and any other territories as we see fit from time to time. We may also transfer your data to our overseas offices or to any people listed under Clause 3.1 above, who may be located in or outside of Singapore.</p>
-      </div>
-      <div class="content">
-        <div class="sub-title">Your Consent and Rights</div>
-        <p>By using our service, making an application or visiting our website, you consent to the collection and use of your information and other activities as outlined in this policy.</p>
-        <p>Under the Personal Data Protection Act (the "PDPA"), individuals have the right:</p>
-        <ol>
-          <li>To check whether we hold personal data about you and to access such data;</li>
-          <li>To require us to correct as soon as reasonably practicable any data relating to you that is inaccurate;</li>
-          <li>To ascertain our policies and practices in relation to personal data and the kind of personal data held by us;</li>
-          <li>To object to the use of your personal data for marketing purposes and we shall not use your personal data for marketing purposes after you communicate your objection to us.</li>
-        </ol>
-        <p>You may exercise your opt-out right by notifying us if you wish to object to the use of your personal data for direct marketing purposes.</p>
-        <p>Please send requests for such objections, access to data, correction of data, information regarding policies and practices and kinds of data held, questions or complaints to info@silot.ai.</p>
-        <p>In accordance with the terms of the PDPA, we have the right to and may charge a minimum fee for processing any data access request.</p>
-      </div>
-      <div class="content">
-        <div class="sub-title">Governing Law and Jurisdiction</div>
-        <p>Nothing in this Privacy Policy shall limit the rights of the data subject under the PDPA. This Privacy Policy shall be governed by the laws of Singapore. You agree to submit to the exclusive jurisdiction of the Singapore courts..</p>
-      </div>
-    </div>
+    <iframe
+      :src="url"
+      class="iframeOnTc"
+    />
+
   </div>
 </template>
-<style lang="scss" scoped>
-.tandc {
-  .wrapper {
-    width: 90%;
-    margin: 0 auto;
-    text-align: center;
-    .update-time {
-      margin-top: 20px;
-      font-size: 15px;
-      color: #929292;
-      .k {
-        float: left;
-      }
-      .v {
-        float: right;
-      }
-    }
-    .title {
-      font-size: 21px;
-    }
-    .content {
-      font-size: 15px;
 
-      .sub-title {
-        font-size: 17px;
-        color: #000;
-        margin: 20px 0;
-      }
-      p {
-        text-align: left;
-      }
-      li {
-        margin-left: 20px;
-        text-align: left;
-        list-style-type: lower-alpha;
-      }
-      ol.sub-list li {
-        list-style-type: lower-roman;
-      }
-    }
-  }
-}
-</style>
 <script>
+import { mapState } from "vuex";
+
 export default {
   data() {
     return {};
+  },
+  computed: {
+    ...mapState({
+      url: state =>
+        state.deviceType === "APP"
+          ? state.WebUrl + "/#/tc-web"
+          : location.href.replace("tc", "tc-web")
+    })
   },
   mounted() {
     window.scrollTo(0, 0);
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.iframeOnTc {
+  height: calc(100vh - 44px);
+  width: 100%;
+  overflow: hidden;
+  border: none;
+}
+</style>
+
