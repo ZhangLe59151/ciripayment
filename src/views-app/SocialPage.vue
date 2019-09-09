a2c747d78b88529c5b26d0848298cc33fc485b9b
le.zhang <le.zhang@silot.ai>
2019-09-09 13:42:48 +0800

    add i18n into Social Page

--- src/views-app/SocialPage.vue
+++ src/views-app/SocialPage.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="socialPage">
     <div class="whiteBackground">
-      <div class="title">Connect your business to big opportunities.</div>
+      <div class="title">{{ $t('Social.title') }}</div>
       <div class="backgroundImg"></div>
-      <van-button class="joinBtn">Join The Community</van-button>
+      <van-button class="joinBtn">{{ $t('Social.joinBtnTitle') }}</van-button>
     </div>
   </div>
 </template>
@@ -26,7 +26,6 @@ export default {
   background-color: black;
 
   .whiteBackground {
-    position: relative;
     height: 100vh;
     width: 100%;
     background-color: white;
@@ -34,28 +33,25 @@ export default {
   }
 
   .title {
-    position: absolute;
-    margin: 92px 16px 0 16px;
+    padding-top: 68px;
+    margin: 0 16px 0 16px;
     width: 328px;
     height: 72px;
     text-align: center;
-    font-size: 24;
+    font-size: 24px;
     font-weight: bold;
   }
 
   .backgroundImg {
-    position: absolute;
-    background: url("../../assets/imgs/social/Social5Icon.png") no-repeat;
+    margin: 32px 30px 0 30px;
+    background: url("../assets/imgs/social/Social5Icon.png") no-repeat;
     background-size: contain;
     height: 360px;
     width: 300px;
   }
 
   .joinBtn {
-    position: absolute;
-    bottom: 44px;
-    left: 16px;
-    right: 16px;
+    margin: 32px 16px 0 16px;
     width: 328px; //calc(100%-32px);
     font-size: 14px;
     background-color: #ffa702;
